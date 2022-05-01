edb07c0cc0e8453e7ae0453094095ecd8f4421c8
robert-drop-table <85031828+robert-drop-table@users.noreply.github.com>
2022-05-01 22:00:32 +0100

    Added child classes

--- akin/minhash.py
+++ akin/minhash.py
@@ -1,3 +1,4 @@
+import heapq
 import numpy as np
 import mmh3
 
@@ -110,3 +111,87 @@ class MinHash:
             hashed_shingle = mmh3.hash128(shingle, seed)
 
         return hashed_shingle
+
+
+class MultiHash(MinHash):
+    def __init__(self):
+        super().__init__()
+
+    def multi_hash(self, texts):
+        """ Generates a texts minhash signature using multi-hash method.
+
+        Uses i random hashes for j permutations selecting the minimum hash value
+        each time to build each texts hash signature.
+
+        Slower but more stable than bottom-k hash method.
+
+        Args:
+            texts(list, np.array): Iterable containing text content of each document.
+
+        Returns:
+            list: List of text signatures generated using bottom-k neighbours method.
+
+        """
+        shingles = self._k_shingles(texts)
+        hash_seeds = np.random.randint(low=1, high=100000000, size=self.permutations)
+
+        signatures = []
+        for document in shingles:
+            signature = []
+            for seed in np.nditer(hash_seeds):
+                min_value = None
+
+                for shingle in document:
+                    hash_value = self._hashing(shingle, int(seed))
+
+                    if not min_value:
+                        min_value = hash_value
+                    elif min_value > hash_value:
+                        min_value = hash_value
+
+                signature.append(min_value)
+
+            signatures.append(signature)
+
+        return np.array(signatures)
+
+
+class BottomK(MinHash):
+    def __init__(self):
+        super().__init__()
+
+    def k_smallest_hash(self, texts):
+        """ Generates a texts minhash signature using k smallest neighbours method.
+
+        Uses a single random hash to simulate a shuffle of each text's shingles.
+        Then selecting i-smallest minimum hash values for j permutations.
+
+        Faster but less stable than multi hash method.
+
+        Args:
+            texts (list, np.array): Iterable containing text content of each document.
+
+        Returns:
+            list: List of text signatures generated using k smallest neighbours method.
+
+        """
+        shingles = self._k_shingles(texts)
+
+        signatures = []
+        for document in shingles:
+            signature = []
+            # Uses a heap queue to make finding the n smallest values more efficient.
+            heapq.heapify(signature)
+
+            if len(document) <= self.permutations:
+                raise ValueError(
+                    'N permutations must not be >= n shingles for k_smallest_values method'
+                )
+
+            for shingle in document:
+                hashed_shingle = self._hashing(shingle, self.seed)
+                heapq.heappush(signature, hashed_shingle)
+
+            signatures.append(heapq.nsmallest(self.permutations, signature))
+
+        return np.array(signatures)
