c066643dba71560c3b05604263d6aadd10c56f24
robert-drop-table <85031828+robert-drop-table@users.noreply.github.com>
2022-05-07 20:59:27 +0100

    Added dictionary array

--- akin/_data_structures.py
+++ akin/_data_structures.py
@@ -1,4 +1,28 @@
 
+class DictionaryArray:
+    def __init__(self, n_arrays):
+        if n_arrays < 1:
+            raise ValueError('Number of arrays must be an integer of 1 or greater')
 
-class BiDirectionalArray:
-    pass
+        self._hash_arrays = [{}] * n_arrays
+
+    def update(self, array, key, value=None):
+        pass
+
+    def remove_key(self, array, key):
+        del self._hash_arrays[array][key]
+
+    def remove_value(self, array, key, value):
+        pass
+
+
+class BiDirectionalDict:
+    def __init__(self):
+        pass
+
+
+class BiDirectionalArray(DictionaryArray):
+    def __init__(self, n_arrays):
+        super(BiDirectionalArray, self).__init__(n_arrays)
+        self._hash_arrays = [BiDirectionalDict()] * n_arrays
+
