b099b04b491ac654c7040319f5eb8ab3553bb76d
robert-drop-table <85031828+robert-drop-table@users.noreply.github.com>
2022-05-21 22:17:10 +0100

    Added and ran dictionary array tests

--- akin/_data_structures.py
+++ akin/_data_structures.py
@@ -1,4 +1,3 @@
-from collections.abc import MutableMapping
 
 
 class DictionaryArray:
@@ -6,12 +5,15 @@ class DictionaryArray:
         if n_arrays < 1:
             raise ValueError('Number of arrays must be an integer of 1 or greater')
 
+        self.n_arrays = n_arrays
         self._hash_arrays = [{}] * n_arrays
 
     def update(self, array, key, value=None):
         if value:
             if self._hash_arrays[array].get(key):
                 self._hash_arrays[array][key].append(value)
+            else:
+                self._hash_arrays[array][key] = [value]
         else:
             if not self._hash_arrays[array].get(key):
                 self._hash_arrays[array][key] = []
@@ -21,45 +23,6 @@ class DictionaryArray:
 
     def remove_value(self, keys, value):
         for i, key in enumerate(keys):
-            self._hash_arrays[i][key].remove(value)
+            self._hash_arrays[i][key] = [v for v in self._hash_arrays[i][key] if v != value]
             if not self._hash_arrays[i][key]:
                 del self._hash_arrays[i][key]
-
-
-class BiDirectionalDict(MutableMapping):
-    def __init__(self, *args, **kwargs):
-        super.__init__(*args, **kwargs)
-        self.store = dict()
-        self.inverse_store = dict()
-
-    def __getitem__(self, item):
-        return self.store.get(item)
-
-    def __setitem__(self, key, value=None):
-        if value is None:
-            value = []
-        self.store[key] = value
-
-        for i in value:
-            if self.inverse_store[value]:
-                self.inverse_store[value].append(key)
-            else:
-                self.inverse_store[value] = [key]
-
-    def __delitem__(self, value):
-        keys = self.inverse_store[value]
-        del self.inverse_store[value]
-
-        for key in keys:
-            self.store[key].remove(value)
-            if not self.store[key]:
-                del self.store[key]
-
-    def __len__(self):
-        pass
-
-    def __iter__(self):
-        pass
-
-    def __repr__(self):
-        pass
--- tests/test_data_structures.py
+++ tests/test_data_structures.py
@@ -15,3 +15,25 @@ def test_dictionary_array():
 
     dictionary_array.update(3, 'test_key', 'test_value')
     assert dictionary_array._hash_arrays[3] == {'test_key': ['test_value']}
+
+    dictionary_array.update(3, 2, 2)
+    assert dictionary_array._hash_arrays[3] == {'test_key': ['test_value'], 2: [2]}
+
+    dictionary_array.update(3, 2, 3)
+    assert dictionary_array._hash_arrays[3] == {'test_key': ['test_value'], 2: [2, 3]}
+
+    dictionary_array.update(3, 4, 3)
+    dictionary_array.update(3, 4, 4)
+    dictionary_array.update(3, 4, 6)
+    dictionary_array.update(3, 5, 4)
+    dictionary_array.update(3, 5, 6)
+    assert dictionary_array._hash_arrays[3] == {'test_key': ['test_value'], 2: [2, 3], 4: [3, 4, 6], 5: [4, 6]}
+
+    dictionary_array.remove_key(3, 'test_key')
+    assert dictionary_array._hash_arrays[3] == {2: [2, 3], 4: [3, 4, 6], 5: [4, 6]}
+
+    dictionary_array.update(3, 2, 2)
+
+    dictionary_array.remove_value([2], 2)
+    assert dictionary_array._hash_arrays[3] == {2: [3], 4: [3, 4, 6], 5: [4, 6]}
+
