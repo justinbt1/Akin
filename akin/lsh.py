f3857298002de40c1c35528f300ffe8bc08b9e85
robert-drop-table <85031828+robert-drop-table@users.noreply.github.com>
2022-05-29 23:12:22 +0100

    Updated update & remove methods to use new data structure

--- akin/lsh.py
+++ akin/lsh.py
@@ -1,7 +1,9 @@
-from collections import defaultdict
+from collections import Counter
 from copy import copy
 import numpy as np
 import mmh3
+
+from akin import DictionaryArray
 
 
 class LSH:
@@ -13,61 +15,74 @@ class LSH:
 
     """
 
-    def __init__(self, minhash=None, labels=None, no_of_bands=None, seed=1):
+    def __init__(self, no_of_bands=None, seed=1):
         """ Initialize the LSH object.
 
         Args:
-            minhash (np.array): Object returned by MinHash class.
-            labels (list, np.array): Iterable, array or pandas series containing labels.
             no_of_bands (int): Number of bands to break minhash signature into.
 
         """
-        # Create default variables
         self.no_of_bands = no_of_bands
         self.seed = seed
-        self._buckets = defaultdict(list)
-        self._i_bucket = defaultdict(list)
+        self._buckets = DictionaryArray(no_of_bands)
         self.permutations = None
 
-        # Run methods if minhash and labels provided
-        if minhash and labels:
-            self.permutations = minhash.permutations
-            self._lsh(minhash.signatures, labels)
-        elif minhash:
-            raise ValueError(
-                'labels cannot be None if LSH initialised with minhash object.'
-            )
-        elif labels:
-            raise ValueError(
-                'minhash object cannot be None if LSH initialised with labels.'
-            )
-
-    def _lsh(self, signatures, labels):
+    def _lsh(self, signatures):
         """ Break signatures into bands and hash components to buckets.
 
         Args:
             signatures (np.array): MinHash signature Matrix.
-            labels (list): List of labels for MinHash signatures.
 
         """
-        if not self.no_of_bands:
-            self.no_of_bands = self.permutations // 2
+        for signature in signatures:
+            hashable_signature = tuple(hash_value for hash_value in signature)
 
-        for label, signature in zip(labels, signatures):
             bands = np.hsplit(
-                np.array(signature), self.no_of_bands
+                np.array(signature),
+                self.no_of_bands
             )
-            for band in bands:
-                bucket_id = mmh3.hash64(tuple(band), self.seed)[0]
-                self._buckets[bucket_id].append(label)
-                self._i_bucket[label].append(bucket_id)
 
-    def _candidate_duplicates(self, bucket_ids, label, sensitivity, jaccard):
+            for band_id, band in enumerate(bands):
+                bucket_id = mmh3.hash64(tuple(band), self.seed)[0]
+                yield band_id, bucket_id, hashable_signature
+
+    def update(self, minhash_signatures):
+        """ Updates LSH object with new MinHash matrix and labels.
+
+        Args:
+            minhash_signatures(np.array): MinHash object containing new minhash signatures to
+                add to LSH object.
+
+        """
+        if not self.permutations:
+            self.permutations = minhash_signatures.shape[1]
+        else:
+            if minhash_signatures.shape[1] != self.permutations:
+                raise IndexError(
+                    f'Number of permutations in minhash must be {self.permutations} to match LSH model.'
+                )
+
+        if not self.no_of_bands:
+            self.no_of_bands = minhash_signatures[0].shape[1] // 2
+
+        band_id, bucket_id, hashable_signature = self._lsh(minhash_signatures)
+        self._buckets.update(band_id, key=bucket_id, value=hashable_signature)
+
+    def remove(self, minhash_signatures):
+        """ Remove label and associated text signature from model.
+
+        Args:
+            minhash_signatures (str, int, float): Label for text to be removed from model.
+
+        """
+        band_id, bucket_id, hashable_signature = self._lsh(minhash_signatures)
+        self._buckets.remove_value(band_id, key=bucket_id, value=hashable_signature)
+
+    def _candidate_duplicates(self, signature, candidates, sensitivity, jaccard_threshold):
         """ Identify candidate duplicates and check Jaccard Similarity.
 
         Args:
-            bucket_ids (list): List of bucket ids.
-            label (str, int, float): Text label.
+            candidates (list): List of bucket ids.
             sensitivity (int): Number of identical buckets two ids must occur
                 in to be considered a near duplicate pair.
             jaccard (float): Minimum Jaccard Similarity for documents to be
@@ -77,66 +92,28 @@ class LSH:
             List: Near duplicate document ids.
 
         """
-        candidates = defaultdict(int)
-        # Retrieve candidate duplicate pairs from model.
-        for bucket_id in bucket_ids:
-            matches = copy(self._buckets.get(bucket_id))
-            matches.remove(label)
-            for match in matches:
-                candidates[match] += 1
-
+        """
         # Apply sensitivity threshold.
         if sensitivity > 1:
             for key in list(candidates):
                 if candidates[key] < sensitivity:
                     del candidates[key]
+        """
 
         # Apply Jaccard threshold and unzip pairs.
-        if jaccard:
-            for key in list(candidates):
-                jaccard_ratio = candidates[key] / self.no_of_bands
-                if jaccard_ratio < jaccard:
-                    del candidates[key]
+        matches = []
 
-        candidates = list(candidates)
+        if jaccard_threshold:
+            for candidate in candidates:
+                intersection = len(set(signature) & set(candidate))
+                union = len(set(signature) | set(candidate))
+                jaccard_ratio = intersection / union
+                if jaccard_ratio < jaccard_threshold:
+                    matches.append(candidate)
 
-        return candidates
+        return matches
 
-    def update(self, minhash, new_labels):
-        """ Updates LSH object with new MinHash matrix and labels.
-
-        Args:
-            minhash (minhash): MinHash object containing new minhash signatures to
-                add to LSH object.
-            new_labels (list): List of new labels to add to LSH object.
-
-        """
-        if self._i_bucket:
-            # Check if texts already exist in index.
-            if set(
-                    self._i_bucket.keys()
-            ).intersection(
-                set(new_labels)
-            ) != set():
-                raise ValueError(
-                    'At least one provided label already exists in model.'
-                )
-
-            if self.permutations != minhash.permutations:
-                raise ValueError(
-                    'Number of permutations in minhash must be {} to match LSH model.'.format(
-                        self.permutations
-                    )
-                )
-
-        else:
-            # Create parameters for new model.
-            self.permutations = minhash.permutations
-
-        # Update model.
-        self._lsh(minhash.signatures, new_labels)
-
-    def query(self, label, min_jaccard=None, sensitivity=1):
+    def query(self, minhash_signature, min_jaccard=None, sensitivity=1):
         """ Returns near duplicates from model.
 
         Takes a provided text label and returns a list of labels for texts whose
@@ -145,7 +122,7 @@ class LSH:
         Can be used to create a recommendation model.
 
         Args:
-            label (str, int, float): Label of text for which to return near duplicates.
+            minhash_signature (str, int, float): Label of text for which to return near duplicates.
             min_jaccard (float): Minimum Jaccard Similarity for texts to be returned as
                 near duplicates.
             sensitivity (int): Number of unique buckets two ids must co-occur in to be
@@ -156,51 +133,29 @@ class LSH:
 
         """
         if sensitivity > self.no_of_bands:
-            raise ValueError(
-                'Sensitivity must be <= no of bands.'
-            )
+            raise ValueError('Sensitivity must be <= no of bands.')
 
-        buckets = self._i_bucket.get(label)
-
-        if not buckets:
-            raise KeyError(
-                'Label {} does not exist in model'.format(label)
-            )
-
-        return self._candidate_duplicates(
-            buckets, label, sensitivity, min_jaccard
+        bands = np.hsplit(
+            np.array(minhash_signature),
+            self.no_of_bands
         )
 
-    def remove(self, label):
-        """ Remove label and associated text signature from model.
+        hashable_signature = tuple(hash_value for hash_value in minhash_signature)
 
-        Args:
-            label (str, int, float): Label for text to be removed from model.
+        candidates_set = set()
+        for band_id, band in enumerate(bands):
+            bucket_id = mmh3.hash64(tuple(band), self.seed)[0]
+            candidates = self._buckets.get(band_id, bucket_id)
+            candidates_set.update(candidates)
 
-        """
-        buckets = self._i_bucket.get(label)
+        candidates_set.remove(hashable_signature)
 
-        if not buckets:
-            raise KeyError(
-                'Label {} does not exist in model.'.format(label)
-            )
-
-        for bucket in buckets:
-            self._buckets[bucket].remove(label)
-
-            if not self._buckets[bucket]:
-                del self._buckets[bucket]
-
-        del self._i_bucket[label]
-
-    def contains(self):
-        """ Returns a list of all labels contained in the model.
-
-        Returns:
-             List: All labels for texts contained in the model.
-
-        """
-        return list(self._i_bucket)
+        return self._candidate_duplicates(
+            hashable_signature,
+            candidates_set,
+            sensitivity,
+            min_jaccard
+        )
 
     def adjacency_list(self, min_jaccard=None, sensitivity=1):
         """ Returns adjacency list.
