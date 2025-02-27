cb05435b28537b002c8a1e29838b07c6dada9345
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2025-02-27 21:40:13 +0000

    Updated packaging

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup
 
+description = 'Akin is a Python library for detecting near-duplicate texts using min-hashing and locality sensitive hashing.'
+
 readme = open('README.md', 'r')
 long_description = readme.read()
 readme.close()
 
+project_urls = {
+  'Homepage': 'https://github.com/justinbt1/Akin'
+}
+
 setup(
     name='akin',
-    version='0.1.0',
+    version='1.0.0',
+    description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=['akin'],
-    install_requires=['numpy', 'cython', 'mmh3']
+    install_requires=['numpy', 'cython', 'mmh3', 'tqdm'],
+    project_urls=project_urls
 )
